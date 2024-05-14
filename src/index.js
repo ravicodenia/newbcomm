64faf5640c0554c84b1a72f1ee13f25087c06d60
ravicodenia <ravisrivastava@Ravis-Air.Dlink>
2024-05-14 17:06:56 +0530

    cs

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
+ 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
